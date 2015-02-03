ea3533fa978a670a499687afebb9438e04202db6
unknown <xgb10124@CIS-SPECIAL-09.ds.strath.ac.uk>
2015-02-03 13:42:28 +0000

    Initial files

---
+++ Kinect/Video Testing/Kinect_Record.m
@@ -0,0 +1,45 @@
+utilpath = fullfile(matlabroot, 'toolbox', 'imaq', 'imaqdemos', ...
+    'html', 'KinectForWindows');
+addpath(utilpath);
+
+vid2 = videoinput('kinect',2,'Depth_640x480'); % Depth camera
+srcDepth = getselectedsource(vid2);
+
+fnum = 1200;
+
+vid2.FramesPerTrigger = 1;
+vid2.TriggerRepeat = fnum;
+triggerconfig(vid2,'manual');
+start(vid2);
+
+vidHeight = 480;
+vidWidth = 640;
+
+s = struct('cdata',zeros(vidHeight,vidWidth,3,'uint8'),...
+    'colormap',[]);
+t = struct('cdata',zeros(vidHeight,vidWidth,3,'uint8'),...
+    'colormap',[]);
+u = struct('cdata',zeros(vidHeight,vidWidth,3,'uint8'),...
+    'colormap',[]);
+
+starttime = clock;
+
+for i = 1:fnum+1
+    % Trigger both objects.
+    trigger(vid2)
+    
+    [imgDepthAll, ts_depthAll, metaData_DepthAll] = getdata(vid2);
+    
+    s(i).cdata = imgDepthAll;
+  %  t(i).cdata = ts_depthAll;
+  %  u(i).cdata = metaData_DepthAll;
+end
+
+endtime = clock;
+
+s = ApproximateMedian(s);
+% If you don't stop you can't start video acquisition again. it will give
+% error when you try to start again. in this case call fnc below
+stop(vid2);
+
+%close(vidObj);
